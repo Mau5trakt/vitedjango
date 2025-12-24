8f6f4e887234784e36e8bac2706bcfd4e2ec39be
Enrique Mauricio Alemany Torres <80871251+Mau5trakt@users.noreply.github.com>
2025-12-24 20:06:16 +0000

    create blog

--- core/models.py
+++ core/models.py
@@ -1,5 +1,14 @@
 from django.conf import settings
 from django.db import models
+import uuid
+from django.contrib.auth.models import User
+from tinymce import models as tinymce_models
+from taggit.managers import TaggableManager
+from .fields import HexField
+
+class PublishedArticlesManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Article.Status.PUBLISHED)
 
 # Create your models here.
 class Employee(models.Model):
@@ -15,3 +24,31 @@ class Employee(models.Model):
     name = models.CharField(max_length=100)
     department = models.CharField(max_length=20, choices=DEPARTMENT_CHOICES)
     salary = models.PositiveIntegerField()
+
+
+
+class Article(models.Model):
+    class Status(models.TextChoices):
+        ARCHIVED = "A", "Archived"
+        DRAFTED = "D", "Drafted"
+        PUBLISHED = "P", "Published"
+    
+
+    author = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    exceprt = models.CharField(max_length=140)
+    body = tinymce_models.HTMLField()
+    status = models.CharField(max_length=1, choices=Status, default=Status.PUBLISHED)
+    publication_time = models.DateTimeField("Creation time", auto_now_add=True)
+    last_modify_time = models.DateTimeField("Modify time", auto_now=True)
+    published = PublishedArticlesManager()
+    category = models.CharField(max_length=30)
+    color = HexField(verbose_name="Banne color")
+    
+    tags = TaggableManager()
+
+
+    class Meta:
+        verbose_name = "Article"
+        verbose_name_plural = "Articles"
+        ordering = ['-publication_time']
